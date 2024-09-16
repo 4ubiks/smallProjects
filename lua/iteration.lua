fb4b79e5cd5d3d45cdc1bfc4601aa351d958c59d
4ubiks <jaharris20@liberty.edu>
2024-09-16 11:17:56 -0400

    m350 exam 2

---
+++ lua/iteration.lua
@@ -0,0 +1,16 @@
+-- prints the sum of 'n' times each natural number. stops when the sum is greater than 'intStop'
+-- used to help find a recursive solution in discrete mathematics
+
+io.write("Enter number to multiply x integers by: ")
+local base = io.read('n')
+
+io.write('\nEnter the integer to stop at: ')
+local intStop = io.read('n')
+
+for i=0, 150 do
+    io.write("\n7 times ", i, "= ")
+    io.write(i * base)
+    if (i * base) > intStop then
+        break
+    end
+end
