6505b27788cf0acfb20695e929af281b59ed8e7c
4ubiks <jaharris20@liberty.edu>
2025-01-08 14:40:41 -0700

    fixed for spaces

--- c/hexAscii/conv.c
+++ c/hexAscii/conv.c
@@ -3,27 +3,39 @@
 #include <stdlib.h>
 
 int main(){
-	printf("ascii: ");
-
 	char str[1024];
 
-	strcpy(str, "48454C4C4F");
+	printf("hex: ");
+	fgets(str, sizeof(str), stdin);
+	str[strcspn(str, "\n")] = '\0'; 						// gets rid of "\n", if exists
+	
 	printf("original: %s", str);
 	printf("\n");
 	printf("\n");
 
+	// get rid of spaces	
 	int length = strlen(str);
+	int j = 0;
+	for (int i=0; i < length; i++){ 						// gets rid of spaces in hex
+		if (str[i] != ' '){
+			str[j] = str[i];
+			j++;
+		}
+	}
+	str[j] = '\0';
+	
+	length = strlen(str);									// redeclare length, after fixes
+	if (length % 2 != 0){printf("invalid"); return 0;} 		// quit if not even
 
-	if (length % 2 != 0){printf("invalid"); return 0;}
+	
 	char ascii;
 	for (int i=0; str[i] != 0; i++){
 		char new[3];
-
+		
 		new[0] = str[i];
 		i++;
 		new[1] = str[i];
 		new[2] = '\0';
-		//printf("%s\n", new);
 
 		int dec = (int)strtol(new, NULL, 16);
 		ascii = (char)dec;
