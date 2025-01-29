4a8cc938d5c56fda11dfcfc664085fa9d2692635
4ubiks <jaharris20@liberty.edu>
2025-01-29 14:17:56 -0500

    yay

--- c/weConvert.c
+++ c/weConvert.c
@@ -8,24 +8,26 @@ int main() {
 	for (int i=0; i < 256; i++){
 		upper=(i >> 4);
 		lower=(i&0x0f);
-		if (upper > 10 && upper < 16){
-			upper+=87;
-		}
-
-		else if (upper > 0 && upper < 10){
+		
+		if (upper < 10){
 			upper+=48;
 		}
 
-		if (lower > 10 && lower < 16){
-			lower +=87;
+		else {
+			upper+=87;
 		}
 
-		else if (lower > 0 && lower < 10){
-			lower+=48;
+		if (lower < 10){
+			lower +=48;
+		}
+
+		else {
+			lower+=87;
 		}
 		
 		putchar(upper);
 		putchar(lower);
 		printf(" ");
 	}	
+	printf("\n");
 }
