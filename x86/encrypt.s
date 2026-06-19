adeff12066099ac53b7ca2f880e97b0469a6e9a4
Jack Harris <144192094+4ubiks@users.noreply.github.com>
2026-06-18 20:17:02 -0400

    added user input and error checking for your key

--- x86/encrypt.s
+++ x86/encrypt.s
@@ -1,53 +1,86 @@
-section .data
-  head db "ghp_"
-  head_len equ $ - head
+	section .data
+	    head db "ghp_"
+	    head_len equ $ - head
 
-  sectB db "xxxxxxxxx"
-  sectB_len equ $ - sectB
+	    sectB db "xxxxxxxxx"
+	    sectB_len equ $ - sectB
 
-  sectU db "xxxxxxxxx"
-  sectB_len equ $ - sectU
+	    sectU db "xxxxxxxxx" 
+	    sectU_len equ $ - sectU
 
-  sectK db "xxxxxxxxx"
-  sectK_len equ $ - sectK
+	    sectK db "xxxxxxxxx"
+	    sectK_len equ $ - sectK
 
-  sectI db "xxxxxxxxx"
-  sectI_len equ $ - sectI
+	    sectI db "xxxxxxxxx", 0xA
+	    sectI_len equ $ - sectI
 
-section .text
-  global _start
+	    key db "key", 10
+	    key_len equ $ - key
 
-_start:
-  mov rax, 1    ; sys_write
-  mov rdi, 1    ; stdout
-  mov rsi, head
-  mov rdx, head_len
-  syscall
+	section .bss
+	    buffer resb 16
 
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, sectU
-  mov rdx, sectU_len
-  syscall
+	section .text
+	    global _start
 
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, sectB
-  mov rdx, sectB_len
-  syscall
+	_start:
+	;; read in key
+	    mov rax, 0
+	    mov rdi, 0
+	    mov rsi, buffer
+	    mov rdx, 16 
+	    syscall
+	    
+	    mov rcx, key
 
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, sectI
-  mov rdx, sectI_len
-  syscall
+	.compare:
 
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, sectK
-  mov rdx, sectK_len
-  syscall
+	;; compare string char X
+	    mov rax, [rcx]
+	    cmp [rsi], al
+	    jne .exit
 
-  mov rax, 60
-  xor rdi, rdi
-  syscall
+	;; go to next char of both
+	    inc rsi
+	    inc rcx
+
+	;; if rsi is empty, jump to print
+	    cmp byte [rsi], 0
+	    je .print
+	    jmp .compare
+	
+	.print:
+	    mov rax, 1          ; sys_write
+	    mov rdi, 1          ; stdout
+	    mov rsi, head
+	    mov rdx, head_len
+	    syscall
+
+	    mov rax, 1
+	    mov rdi, 1
+	    mov rsi, sectU
+	    mov rdx, sectU_len
+	    syscall
+
+	    mov rax, 1
+	    mov rdi, 1
+	    mov rsi, sectB
+	    mov rdx, sectB_len
+	    syscall
+
+	    mov rax, 1
+	    mov rdi, 1
+	    mov rsi, sectI
+	    mov rdx, sectI_len
+	    syscall
+
+	    mov rax, 1
+	    mov rdi, 1
+	    mov rsi, sectK
+	    mov rdx, sectK_len
+	    syscall
+
+	.exit:
+	    mov rax, 60         ; sys_exit
+	    xor rdi, rdi
+	    syscall
